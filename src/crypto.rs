37de4e5ff28f857c5e29d2551ae8449f3425ec81
David <david.smitmanis@gmail.com>
2015-09-05 18:42:29 +0200

    Initial commit

---
+++ src/crypto.rs
@@ -0,0 +1,58 @@
+use sodiumoxide::crypto::pwhash;
+use sodiumoxide::crypto::secretbox;
+use std::string::String;
+
+pub fn get_key(password: &str, salt: pwhash::Salt) -> secretbox::Key {
+    let password_bytes = password.as_bytes();
+    let mut key = secretbox::Key([0; secretbox::KEYBYTES]);
+    {
+        let secretbox::Key(ref mut key_bytes) = key;
+        pwhash::derive_key(key_bytes, password_bytes, &salt,
+                           pwhash::OPSLIMIT_INTERACTIVE,
+                           pwhash::MEMLIMIT_INTERACTIVE).unwrap();
+    }
+    key
+}
+
+pub fn encrypt_password(plaintext: &str, key: &secretbox::Key,
+                        nonce: &secretbox::Nonce) -> Vec<u8> {
+    let plaintext_bytes = plaintext.as_bytes();
+    return secretbox::seal(&plaintext_bytes, &nonce, &key);
+}
+
+pub fn decrypt_password(ciphertext: &Vec<u8>, key: &secretbox::Key,
+                        nonce: &secretbox::Nonce) -> Result<String, ()> {
+    let decrypted = try!(secretbox::open(&ciphertext, &nonce, &key));
+    match String::from_utf8(decrypted) {
+        Ok(decrypted_text) => Ok(decrypted_text),
+        Err(_) => Err(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{get_key, encrypt_password, decrypt_password};
+    use sodiumoxide::crypto::pwhash;
+    use sodiumoxide::crypto::secretbox;
+
+    /// Test that get_key always returns the same key given the same
+    /// password, and that its result can be used as a key for
+    /// encrypt/decrypt
+    #[test]
+    fn test_key_derivation_and_encryption() {
+        let password = "super secret password";
+        let plaintext = "secret content";
+        let nonce = secretbox::gen_nonce();
+        let key_salt = pwhash::gen_salt();
+        let encrypted: Vec<u8>;
+        {
+            let key = get_key(password, key_salt);
+            encrypted = encrypt_password(plaintext, &key, &nonce);
+        }
+        let key = get_key(password, key_salt);
+        let decrypted = decrypt_password(&encrypted, &key, &nonce)
+            .unwrap();
+        assert_eq!(plaintext.to_string(), decrypted);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate sodiumoxide;
+mod crypto;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    // let key = crypto::get_key("dsds");
+    // let sodiumoxide::crypto::secretbox::Key(ref bleh) = key;
+    // println!("key: {}", str::from_utf8(bleh).unwrap());
 }
