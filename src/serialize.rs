c3242d2bed1f98554e38e38c96de859e6ae4f40a
David <david.smitmanis@gmail.com>
2015-09-11 22:06:04 +0200

    Add methods for manupulating accounts

--- src/serialize.rs
+++ src/serialize.rs
@@ -11,22 +11,18 @@ pub struct PasswordData {
 }
 
 #[derive(Debug, PartialEq)]
-pub enum PasswordRetrieveError {
+pub enum AccountError {
     SyntaxError,
-    AccountNotFound
+    AccountNotFound,
+    AccountAlreadyExists
 }
 
-use self::PasswordRetrieveError::*;
+use self::AccountError::*;
 
 pub fn get_password_data_for(account_name: &str,
-                             accounts: &str,) -> Result<PasswordData, PasswordRetrieveError> {
+                             accounts: &str,) -> Result<PasswordData, AccountError> {
 
-    let mut account_map: AccountMap = match json::decode(accounts) {
-        Ok(accmap) => accmap,
-        Err(_) => {
-            return Err(SyntaxError);
-        }
-    };
+    let mut account_map = try!(get_account_map(accounts));
 
     // Pluck the PasswordData struct from the map to gain ownership of it
     let password_data = match account_map.remove(account_name) {
@@ -37,6 +33,66 @@ pub fn get_password_data_for(account_name: &str,
     };
 
     Ok(password_data)
+}
+
+pub fn add_account(account_name: &str, password_data: PasswordData,
+                   accounts: &str) -> Result<String, AccountError> {
+    let mut account_map = try!(get_account_map(accounts));
+    if account_map.contains_key(account_name) {
+        // TODO: Error type is placeholder
+        Err(AccountAlreadyExists)
+    }
+    else {
+        account_map.insert(account_name.to_string(), password_data);
+        match json::encode(&account_map) {
+            Ok(json_account_map) => Ok(json_account_map),
+            Err(_) => {
+                return Err(SyntaxError);
+            }
+        }
+    }
+}
+
+pub fn change_account(account_name: &str, password_data: PasswordData,
+                      accounts: &str) -> Result<String, AccountError> {
+    let mut account_map = try!(get_account_map(accounts));
+    if !account_map.contains_key(account_name) {
+        Err(AccountNotFound)
+    }
+    else {
+        account_map.insert(account_name.to_string(), password_data);
+        match json::encode(&account_map) {
+            Ok(json_account_map) => Ok(json_account_map),
+            Err(_) => {
+                return Err(SyntaxError);
+            }
+        }
+    }
+}
+
+pub fn remove_account(account_name: &str,
+                      accounts: &str,) -> Result<(), AccountError> {
+    let mut account_map = try!(get_account_map(accounts));
+
+    match account_map.remove(account_name) {
+        Some(pwdata) => pwdata,
+        None => {
+            return Err(AccountNotFound);
+        }
+    };
+
+    Ok(())
+}
+
+fn get_account_map(accounts: &str) -> Result<AccountMap, AccountError> {
+    let account_map: AccountMap = match json::decode(accounts) {
+        Ok(accmap) => accmap,
+        Err(_) => {
+            return Err(SyntaxError);
+        }
+    };
+
+    Ok(account_map)
 }
 
 #[cfg(test)]
@@ -69,14 +125,14 @@ mod test {
     #[test]
     fn test_account_doesnt_exist() {
         let result = get_password_data_for("nosuchaccount", VALID_ACCOUNT_STRUCTURE);
-        assert_eq!(result, Err(PasswordRetrieveError::AccountNotFound));
+        assert_eq!(result, Err(AccountError::AccountNotFound));
     }
 
     #[test]
     fn test_empty_account_structure() {
         let account_structure = "{}";
         let result = get_password_data_for("gmail", account_structure);
-        assert_eq!(result, Err(PasswordRetrieveError::AccountNotFound));
+        assert_eq!(result, Err(AccountError::AccountNotFound));
     }
 
     #[test]
@@ -88,6 +144,32 @@ mod test {
             }
         }";
         let result = get_password_data_for("gmail", account_structure);
-        assert_eq!(result, Err(PasswordRetrieveError::SyntaxError));
+        assert_eq!(result, Err(AccountError::SyntaxError));
+    }
+
+    #[test]
+    fn test_add_new_account() {
+        let pwdata = PasswordData {
+            salt: "dsdsds".to_string(),
+            nonce: "abvcasd".to_string(),
+            password_hash: "dsadsaD".to_string()
+        };
+        let new_json_structure = add_account("twitter", pwdata, VALID_ACCOUNT_STRUCTURE).unwrap();
+        let twitter_password = get_password_data_for("twitter", &new_json_structure).unwrap();
+        assert_eq!(&twitter_password.password_hash, "dsadsaD");
+        let gmail_password = get_password_data_for("gmail", &new_json_structure).unwrap();
+        assert_eq!(&gmail_password.password_hash, "dsadsaFGd");
+    }
+
+    #[test]
+    fn test_change_account() {
+        let pwdata = PasswordData {
+            salt: "dsdsds".to_string(),
+            nonce: "abvcasd".to_string(),
+            password_hash: "dsadsaD".to_string()
+        };
+        let new_json_structure = change_account("gmail", pwdata, VALID_ACCOUNT_STRUCTURE).unwrap();
+        let gmail_password = get_password_data_for("gmail", &new_json_structure).unwrap();
+        assert_eq!(&gmail_password.password_hash, "dsadsaD");
     }
 }
